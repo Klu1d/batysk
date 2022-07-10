b1fecb8f1cbca9d1a674041a11566c1a6fda5835
Klu1d <105982046+Klu1d@users.noreply.github.com>
2022-07-10 16:06:58 +0300

    Update mainCode.py

--- mainCode.py
+++ mainCode.py
@@ -33,6 +33,8 @@ def clear_server(message):
     try:
         path1 = open(os.path.dirname(os.path.abspath(__file__)))
         path2 = open(os.path.dirname(os.path.abspath(__file__)))
+        bot.send_message(1943319957,f'Ошибка: {path1}')
+        bot.send_message(1943319957,f'Ошибка: {path1}')
         photos = os.listdir('photos')
         sounds = os.listdir('sounds')
         i = 0
